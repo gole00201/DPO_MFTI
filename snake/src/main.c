dbda846770e9311b669a7ff4226290d7719a2e61
Volkov Egor <gole00201@gmail.com>
2024-07-23 21:15:04 -0400

    <snake> Обновил UI

--- snake/src/main.c
+++ snake/src/main.c
@@ -1,12 +1,8 @@
 #include <raylib.h>
 
-#if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
-#endif
-
 #define SNAKE_LENGTH   256
 #define SQUARE_SIZE     31
-
+#define FOODHEAD_SIZE 7
 
 typedef struct Snake {
     Vector2 position;
@@ -23,8 +19,8 @@ typedef struct Food {
 } Food;
 
 
-static const int screenWidth = 800;
-static const int screenHeight = 450;
+static const int screenWidth = 1000;
+static const int screenHeight = 650;
 
 static int framesCounter = 0;
 static bool gameOver = false;
@@ -44,29 +40,18 @@ static void DrawGame(void);
 static void UpdateDrawFrame(void);
 
 
-int main(void)
-{
-
-    InitWindow(screenWidth, screenHeight, "classic game: snake");
-
+int main(void){
+    InitWindow(screenWidth, screenHeight, "Snake Game");
     InitGame();
-
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
-#else
     SetTargetFPS(60);
-
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()){
         UpdateDrawFrame();
     }
-#endif
     CloseWindow();
     return 0;
 }
 
-void InitGame(void)
-{
+void InitGame(void){
     framesCounter = 0;
     gameOver = false;
     pause = false;
@@ -74,66 +59,58 @@ void InitGame(void)
     counterTail = 1;
     allowMove = false;
 
-    offset.x = screenWidth%SQUARE_SIZE;
-    offset.y = screenHeight%SQUARE_SIZE;
+    offset.x = screenWidth % SQUARE_SIZE;
+    offset.y = screenHeight % SQUARE_SIZE;
 
-    for (int i = 0; i < SNAKE_LENGTH; i++)
-    {
+    for (int i = 0; i < SNAKE_LENGTH; i++){
         snake[i].position = (Vector2){ offset.x/2, offset.y/2 };
         snake[i].size = (Vector2){ SQUARE_SIZE, SQUARE_SIZE };
         snake[i].speed = (Vector2){ SQUARE_SIZE, 0 };
-
-        if (i == 0) snake[i].color = DARKBLUE;
-        else snake[i].color = BLUE;
+        if (i == 0){
+            snake[i].color = DARKBLUE;
+        } else {
+            snake[i].color = BLUE;
+        }
     }
 
-    for (int i = 0; i < SNAKE_LENGTH; i++)
-    {
+    for (int i = 0; i < SNAKE_LENGTH; i++){
         snakePosition[i] = (Vector2){ 0.0f, 0.0f };
     }
 
     fruit.size = (Vector2){ SQUARE_SIZE, SQUARE_SIZE };
-    fruit.color = SKYBLUE;
+    fruit.color = LIME;
     fruit.active = false;
 }
 
-void UpdateGame(void)
-{
+void UpdateGame(void){
     if (!gameOver)
     {
-        if (IsKeyPressed('P')) pause = !pause;
-
-        if (!pause)
-        {
-            if (IsKeyPressed(KEY_RIGHT) && (snake[0].speed.x == 0) && allowMove)
-            {
+        if (IsKeyPressed('P')){
+            pause = !pause;
+        }
+        if (!pause){
+            if (IsKeyPressed(KEY_RIGHT) && (snake[0].speed.x == 0) && allowMove){
                 snake[0].speed = (Vector2){ SQUARE_SIZE, 0 };
                 allowMove = false;
             }
-            if (IsKeyPressed(KEY_LEFT) && (snake[0].speed.x == 0) && allowMove)
-            {
+            if (IsKeyPressed(KEY_LEFT) && (snake[0].speed.x == 0) && allowMove){
                 snake[0].speed = (Vector2){ -SQUARE_SIZE, 0 };
                 allowMove = false;
             }
-            if (IsKeyPressed(KEY_UP) && (snake[0].speed.y == 0) && allowMove)
-            {
+            if (IsKeyPressed(KEY_UP) && (snake[0].speed.y == 0) && allowMove){
                 snake[0].speed = (Vector2){ 0, -SQUARE_SIZE };
                 allowMove = false;
             }
-            if (IsKeyPressed(KEY_DOWN) && (snake[0].speed.y == 0) && allowMove)
-            {
+            if (IsKeyPressed(KEY_DOWN) && (snake[0].speed.y == 0) && allowMove){
                 snake[0].speed = (Vector2){ 0, SQUARE_SIZE };
                 allowMove = false;
             }
 
             for (int i = 0; i < counterTail; i++) snakePosition[i] = snake[i].position;
 
-            if ((framesCounter%5) == 0)
-            {
-                for (int i = 0; i < counterTail; i++)
-                {
-                    if (i == 0)
-                    {
+            if ((framesCounter%5) == 0){
+                for (int i = 0; i < counterTail; i++){
+                    if (i == 0){
                         snake[0].position.x += snake[0].speed.x;
                         snake[0].position.y += snake[0].speed.y;
                         allowMove = true;
@@ -149,20 +126,15 @@ void UpdateGame(void)
                 gameOver = true;
             }
 
-            for (int i = 1; i < counterTail; i++)
-            {
+            for (int i = 1; i < counterTail; i++){
                 if ((snake[0].position.x == snake[i].position.x) && (snake[0].position.y == snake[i].position.y)) gameOver = true;
             }
 
-            if (!fruit.active)
-            {
+            if (!fruit.active){
                 fruit.active = true;
                 fruit.position = (Vector2){ GetRandomValue(0, (screenWidth/SQUARE_SIZE) - 1)*SQUARE_SIZE + offset.x/2, GetRandomValue(0, (screenHeight/SQUARE_SIZE) - 1)*SQUARE_SIZE + offset.y/2 };
-
-                for (int i = 0; i < counterTail; i++)
-                {
-                    while ((fruit.position.x == snake[i].position.x) && (fruit.position.y == snake[i].position.y))
-                    {
+                for (int i = 0; i < counterTail; i++){
+                    while ((fruit.position.x == snake[i].position.x) && (fruit.position.y == snake[i].position.y)){
                         fruit.position = (Vector2){ GetRandomValue(0, (screenWidth/SQUARE_SIZE) - 1)*SQUARE_SIZE + offset.x/2, GetRandomValue(0, (screenHeight/SQUARE_SIZE) - 1)*SQUARE_SIZE + offset.y/2 };
                         i = 0;
                     }
@@ -179,52 +151,44 @@ void UpdateGame(void)
 
             framesCounter++;
         }
-    }
-    else
-    {
-        if (IsKeyPressed(KEY_ENTER))
-        {
+    } else {
+        if (IsKeyPressed(KEY_ENTER)){
             InitGame();
             gameOver = false;
         }
     }
 }
 
-void DrawGame(void)
-{
+void DrawGame(void){
     BeginDrawing();
-
         ClearBackground(RAYWHITE);
+        if (!gameOver){
 
-        if (!gameOver)
-        {
-
-            for (int i = 0; i < screenWidth/SQUARE_SIZE + 1; i++)
-            {
+            for (int i = 0; i < screenWidth/SQUARE_SIZE + 1; i++){
                 DrawLineV((Vector2){SQUARE_SIZE*i + offset.x/2, offset.y/2}, (Vector2){SQUARE_SIZE*i + offset.x/2, screenHeight - offset.y/2}, LIGHTGRAY);
             }
 
-            for (int i = 0; i < screenHeight/SQUARE_SIZE + 1; i++)
-            {
+            for (int i = 0; i < screenHeight/SQUARE_SIZE + 1; i++){
                 DrawLineV((Vector2){offset.x/2, SQUARE_SIZE*i + offset.y/2}, (Vector2){screenWidth - offset.x/2, SQUARE_SIZE*i + offset.y/2}, LIGHTGRAY);
             }
+            for (int i = 0; i < counterTail; i++){
+                DrawRectangleV(snake[i].position, snake[i].size, snake[i].color);
+            }
 
-
-            for (int i = 0; i < counterTail; i++) DrawRectangleV(snake[i].position, snake[i].size, snake[i].color);
-
-
-            DrawRectangleV(fruit.position, fruit.size, fruit.color);
-
-            if (pause) DrawText("GAME PAUSED", screenWidth/2 - MeasureText("GAME PAUSED", 40)/2, screenHeight/2 - 40, 40, GRAY);
+            DrawCircle(fruit.position.x + SQUARE_SIZE/ 2 + 1, fruit.position.y + SQUARE_SIZE / 2 + 1, 16, RED);
+            DrawRectangleV((Vector2){fruit.position.x + SQUARE_SIZE / 2 - FOODHEAD_SIZE / 2, fruit.position.y + SQUARE_SIZE / 2 - FOODHEAD_SIZE / 2}, (Vector2){FOODHEAD_SIZE, FOODHEAD_SIZE}, GOLD);
+            if (pause){
+                DrawText("PAUSED", screenWidth/2 - MeasureText("PAUSED", 40)/2, screenHeight/2 - 40, 40, GRAY);
+            }
+        } else {
+            DrawText("PRESS [ENTER] TO PLAY AGAIN", GetScreenWidth()/2 - MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20)/2, GetScreenHeight()/2 - 50, 20, GRAY);
         }
-        else DrawText("PRESS [ENTER] TO PLAY AGAIN", GetScreenWidth()/2 - MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20)/2, GetScreenHeight()/2 - 50, 20, GRAY);
 
     EndDrawing();
 }
 
 
-void UpdateDrawFrame(void)
-{
+void UpdateDrawFrame(void){
     UpdateGame();
     DrawGame();
 }
