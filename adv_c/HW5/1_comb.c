4abd23c7cfa0eafd8ee3d4ceb57a5824132fae78
volkov <volkov@crtc.spb.ru>
2024-08-12 16:05:52 +0300

    <HW5> Первое Дз

--- adv_c/HW5/1_comb.c
+++ adv_c/HW5/1_comb.c
@@ -1,54 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-#define MAX_DIGITS 10
+#define MAX_NUM_LEN 100
 
-void count_digits(const char *str, int *heat_map) {
-    for (const char *p = str; *p; p++) {
-        if (*p >= '0' && *p <= '9') {
-            heat_map[*p - '0']++;
+bool isUnique(int number, int *uniqueNumbers, int count) {
+    for (int i = 0; i < count; i++) {
+        if (uniqueNumbers[i] == number) {
+            return false;
         }
     }
+    return true;
 }
 
-unsigned long long choose(unsigned long long n, unsigned long long k) {
-    if (k > n) {
-        return 0;
-    }
-    unsigned long long r = 1;
-    for (unsigned long long d = 1; d <= k; ++d) {
-        r *= n--;
-        r /= d;
-    }
-    return r;
-}
-
-int count_unique_3_digit_numbers(const char *str) {
-    int heat_map[MAX_DIGITS] = {0};
-    int digits[MAX_DIGITS];
-    int num_digits = 0;
-
-    count_digits(str, heat_map);
-    for (int i = 0; i < MAX_DIGITS; i++) {
-        if (heat_map[i] > 0) {
-            digits[num_digits++] = i;
+int main() {
+    char N[MAX_NUM_LEN];
+    scanf("%s", N);
+    int length = strlen(N);
+    int uniqueNumbers[MAX_NUM_LEN * MAX_NUM_LEN] = {0};
+    int uniqueCount = 0;
+    for (int i = 0; i < length - 2; i++) {
+        for (int j = i + 1; j < length - 1; j++) {
+            for (int k = j + 1; k < length; k++) {
+                int number = (N[i] - '0') * 100 + (N[j] - '0') * 10 + (N[k] - '0');
+                if (isUnique(number, uniqueNumbers, uniqueCount)) {
+                    uniqueNumbers[uniqueCount++] = number;
+                }
+            }
         }
     }
-
-    if(num_digits == 1){
-        return 1;
-    }
-    if(num_digits == 2){
-        return 4;
-    }
-
-    return choose(num_digits, 3);
-}
-
-int main(void) {
-    char input[101];
-    scanf("%100s", input);
-    printf("%d\n", count_unique_3_digit_numbers(input));
+    printf("%d\n", uniqueCount);
     return 0;
 }
--- adv_c/HW5/2_zfunc.c
+++ adv_c/HW5/2_zfunc.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-int
-main( void ){
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int main() {
+    char str1[10005], str2[10005];
+    scanf("%s", str1);
+    scanf("%s", str2);
+
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+
+    int maxPrefixSuffix = 0;  // Для первого числа
+    int maxSuffixPrefix = 0;  // Для второго числа
+
+    // 1. Поиск максимального префикса первой строки, являющегося суффиксом второй
+    for (int i = 1; i <= min(len1, len2); i++) {
+        if (strncmp(str1, str2 + len2 - i, i) == 0) {
+            maxPrefixSuffix = i;
+        }
+    }
+
+    // 2. Поиск максимального суффикса первой строки, являющегося префиксом второй
+    for (int i = 1; i <= min(len1, len2); i++) {
+        if (strncmp(str1 + len1 - i, str2, i) == 0) {
+            maxSuffixPrefix = i;
+        }
+    }
+
+    printf("%d %d\n", maxPrefixSuffix, maxSuffixPrefix);
 
     return 0;
 }
